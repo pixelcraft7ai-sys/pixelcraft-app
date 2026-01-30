5e7b06774e9cd0f105e135ec0ea0c290c756cd3e
PixelCraft Deploy <deploy@pixelcraft.app>
2026-01-30 01:57:12 -0500

    Debug: Switch to CommonJS API

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({ status: 'ok', message: 'Simple API is working' });
-}
+module.exports = (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'CJS API is working' });
+};
