2bdc10bd34460cd240dff7cab47123660b84d586
PixelCraft Deploy <deploy@pixelcraft.app>
2026-01-30 17:30:57 -0500

    Debug: Minimal ESM headers API

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-module.exports = (req, res) => {
-  res.end('API is working CJS end');
-};
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('API is working ESM headers');
+}
